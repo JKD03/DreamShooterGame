619525edcb37743e2167f67bf89ec5e38465e1c0
Jaskarandeep Singh <talk2jkds@gmail.com>
2023-06-13 17:43:56 +0530

    Project Files

---
+++ Assets/Scripts/Factory Pattern Test/FactoryPatternDemo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class FactoryPatternDemo
+{
+    
+    private void Start()
+    {
+    ShapeFactory shapeFactory = new ShapeFactory();
+    
+      //get an object of Circle and call its draw method.
+    Shape shape1 = shapeFactory.getShape("CIRCLE");
+
+      //get an object of Rectangle and call its draw method.
+    Shape shape2 = shapeFactory.getShape("RECTANGLE");
+
+      //get an object of Square and call its draw method.
+    Shape shape3 = shapeFactory.getShape("SQUARE");
+
+   }
+}
---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    public Text warningText;
+    public PlayerHealth playerHealth;       
+    public float restartDelay = 5f;            
+
+
+    Animator anim;                          
+    float restartTimer;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("quit command called");
+            //UnityEditor.EditorApplication.isPlaying = false;
+            Application.Quit();
+        }*/
+
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+            
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    public void ShowWarning(float enemyDistance)
+    {
+        warningText.text = string.Format("! {0} m",Mathf.RoundToInt(enemyDistance));
+        anim.SetTrigger("Warning");
+    }
+}
